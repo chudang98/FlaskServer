5b2ba8a43e5e9383433375a292dbacc071fbd082
dang.chu <dang.chu@onemount.com>
2023-03-23 23:56:47 +0700

    edit cors

--- app.py
+++ app.py
@@ -19,11 +19,16 @@ server_api = Flask(__name__)
 server_api.register_blueprint(auth_routes)
 server_api.register_blueprint(project_routes)
 
-server_api.config['CORS_ALLOW_HEADERS'] = '*'
-server_api.config['CORS_METHODS'] = '*'
-server_api.config['CORS_SUPPORTS_CREDENTIALS'] = True
-server_api.config['CORS_ORIGINS'] = '*'
-CORS(server_api, resources=r"/api/*")
+# server_api.config['CORS_ALLOW_HEADERS'] = '*'
+# server_api.config['CORS_METHODS'] = '*'
+# server_api.config['CORS_SUPPORTS_CREDENTIALS'] = True
+# server_api.config['CORS_ORIGINS'] = '*'
+# CORS(server_api, resources=r"/api/*")
+cors = CORS(server_api, resources={r"/api/*": {
+    "origins": "*",
+    "supports_credentials": True
+    }
+  })
 
 if __name__ == "__main__":
     server_api.run(host='0.0.0.0', port=5000,
