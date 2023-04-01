e41cc95039ef6806b31242b2bcd325f2021685a2
dang.chu <dang.chu@onemount.com>
2023-04-01 21:35:02 +0700

    edit image docker

--- utils/docker.py
+++ utils/docker.py
@@ -10,7 +10,7 @@ def create_container(project_id, twitter_url, table_id='canvas-figure-378911.twi
     f'--project_url {twitter_url} --table_id {table_id}',
     detach=False,
     volumes={'/home/chudang98hn/cert/': {'bind': '/app/cert/', 'mode': 'ro'}},
-    network='backend'
+    network='flaskserver_backend'
   )
   logging.warning("Created container !")
   client_docker.close()
