08426e648498d5e37218670eacbbe89096822f02
dang.chu <dang.chu@onemount.com>
2023-04-18 20:34:00 +0700

    update bigquery

--- utils/bigquerry.py
+++ utils/bigquerry.py
@@ -1,14 +1,19 @@
+import logging
+
 from google.cloud import bigquery
 from google.oauth2 import service_account
 
 SA_AUTH = '/app/cert/canvas-figure-bq.json'
 
 def create_client():
+  logging.warning("Start create client BigQuery...")
   path_auth = SA_AUTH
+  logging.warning("Loading credentials...")
   credentials = service_account.Credentials.from_service_account_file(
     path_auth, scopes=["https://www.googleapis.com/auth/cloud-platform"],
   )
   client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+  logging.warning("Created client BigQuery !")
   return client
 
 def query_get_all_user_project(project_id, table='canvas-figure-378911.twitter_crawl.permission'):
@@ -39,6 +44,7 @@ def add_read_project_id_permission(list_prj_ids, email, table='canvas-figure-378
     FROM table
     WHERE project_id in ({condition_qr})
   """
+  logging.warning(f"Query get all project_id permission : {check_permission_qr}")
   existed_permission = bq_client.query(check_permission_qr)
   for project_permistion in existed_permission:
     # TODO: Check email has permisstion for this project_id
@@ -47,10 +53,10 @@ def add_read_project_id_permission(list_prj_ids, email, table='canvas-figure-378
       query_update = f""""
         UPDATE `{table}`
         SET email = ARRAY_CONCAT(email, ['{email}'])
-        WHERE project_id = '{project_permistion}'
+        WHERE project_id = '{project_permistion[0]}'
       """
+      logging.warning(f"Query add email {email} to project_id {project_permistion[0]} : {query_update}")
       bq_client.query(query_update)
-      pass
 
 # TODO: Add project_id permision for first time save or had been deleted.
   prj_existed = [proj[0] for proj in existed_permission]
@@ -60,5 +66,9 @@ def add_read_project_id_permission(list_prj_ids, email, table='canvas-figure-378
         INSERT INTO `{table}`(project_id, email)
         VALUES('{prj_id}', ['{email}'])  
       """
+      logging.warning(f"""
+          Project id {prj_id} is new.
+          Query add new project id {prj_id} for email {email}
+      """)
       bq_client.query(query)
   bq_client.close()
