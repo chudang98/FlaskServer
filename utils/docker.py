e65f92de5c28f28fcaeac86d8957e5570d2d44a8
dang.chu <dang.chu@onemount.com>
2023-05-08 21:17:33 +0700

    edit args

--- utils/docker.py
+++ utils/docker.py
@@ -13,7 +13,7 @@ def create_container(project_id, twitter_url, email, table_id='canvas-figure-378
   project_name = re.search('(?<=\/\/twitter.com\/)([a-zA-Z0-9]*)', twitter_url).group()
   client_docker.containers.run(
     'crawl_app',
-    f'--project_url {twitter_url} --table_id {table_id} --project_id {project_id} --email {email} --name {project_name}_{email}_{datetime.datetime.now(tz).strftime("%y%m%d%H%M%S")}',
+    f'--project_url {twitter_url} --table_id {table_id} --project_id {project_id} --email {email}',
     detach=True,
     volumes={'/home/chudang98hn/cert/': {'bind': '/app/cert/', 'mode': 'ro'}},
     network='flaskserver_backend'
