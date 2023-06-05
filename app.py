de6721be9e7abe4eac95c6ebac313b6d53139329
dang.chu <dang.chu@onemount.com>
2023-06-05 19:30:05 +0700

    edit g

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ logging.warning(MONGODB_URL)
 server_api = Flask(__name__)
 server_api.register_blueprint(auth_routes)
 server_api.register_blueprint(project_routes)
-
+g.scheduler_crawler = init_schedule_crawl()
 
 # CORS(server_api, resources=r"/api/*")
 # CORS(server_api)
@@ -42,7 +42,6 @@ if __name__ == "__main__":
                    debug=True
                         # ssl_context=('/app/cert/cert.pem', '/app/cert/key.pem')
                    )
-    g.scheduler_crawler = init_schedule_crawl()
     # CORS(server_api, resources=r"/api/*")
     # CORS(server_api, resources={r"/api/*": {
     #   "origins": "*",
